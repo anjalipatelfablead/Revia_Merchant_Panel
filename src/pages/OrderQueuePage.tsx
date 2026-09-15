5a7aa31411fb5997ed0386978020f73d338a4db1
anjalipatelfablead <anjalipatel.fablead@gmail.com>
2026-09-15 18:02:21 +0530

    updated deeisgn changes

--- src/pages/OrderQueuePage.tsx
+++ src/pages/OrderQueuePage.tsx
@@ -214,7 +214,7 @@ export const OrderQueuePage: React.FC<{ onNavigate?: (route: NavRoute) => void }
         </div>
       </div>
 
-      <div className="px-4 md:px-0 pb-6 flex-1 flex flex-col gap-4 md:gap-6 overflow-hidden">
+      <div className=" md:px-0  flex-1 flex flex-col gap-4 md:gap-6 overflow-hidden">
         {/* Summary Cards */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 shrink-0">
           {[
